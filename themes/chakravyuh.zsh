58d7daca73cad653001f754f707c66aec28ae158
Krishna <YOUR_GITHUB_EMAIL>
2026-06-30 13:42:32 +0530

    feat: add git branch detection

--- themes/chakravyuh.zsh
+++ themes/chakravyuh.zsh
@@ -5,7 +5,11 @@
 autoload -Uz colors
 colors
 
+# Returns the current Git branch if inside a Git repository
+git_branch() {
+    git branch --show-current 2>/dev/null
+}
 PROMPT='
 %F{220}🪷 %f%F{220}%n%f %F{244}⟢%f %F{220}%m%f
-     %F{39}⟢ %~%f
+     %F{39}⟢ %~ %F{34}$(git_branch)%f
           %F{220}⚔ ❯ %f'
