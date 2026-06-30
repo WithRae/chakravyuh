208fe8f075135ed3f37bacb0b2ce09a716e42dce
Krishna <YOUR_GITHUB_EMAIL>
2026-06-30 13:28:37 +0530

    feat: create first Chakravyuh prompt

---
+++ themes/chakravyuh.zsh
@@ -0,0 +1,11 @@
+# ==========================
+# Chakravyuh Prompt v0.1
+# ==========================
+
+autoload -Uz colors
+colors
+
+PROMPT='
+%F{220}🪷 %f%F{220}%n%f %F{244}⟢%f %F{220}%m%f
+     %F{39}⟢ %~%f
+          %F{220}⚔ ❯ %f'
